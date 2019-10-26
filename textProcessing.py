0ba3e191506fb4336b87a5d3f967482c2313320d
Andrew Reddy <andyreddy16@berkeley.edu>
2019-10-26 12:39:38 -0700

    Working on text processing (removing stopwords and cleanup)

--- textProcessing.py
+++ textProcessing.py
@@ -1,8 +1,14 @@
 import nltk
 import gensim
 import spacy
+nltk.download()
 
 def preprocess_text(corpus):
 	stopwords = nltk.corpus.stopwords
+	tokens = nltk.word_tokenize(corpus)
+	text = nltk.Text(tokens)
+	print(type(text))
+	lemmatizer = nltk.stem.WordNetLemmatizer()
+	processed_text = [lemmatizer.lemmatize(word) for word in text]
 
-	return None
+	return processed_text
